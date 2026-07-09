12e93b575a4780b16eede3efa93df39afd526c8b
KidusAbadi21 <kidusabadi21@gmail.com>
2026-07-10 00:24:00 +0300

    Update UI and app theme

--- lib/subject card/subject_card.dart
+++ lib/subject card/subject_card.dart
@@ -13,57 +13,67 @@ class SubjectCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: () {},
-      child: Container(
-        decoration: BoxDecoration(
-          color: AppColors.card,
-          boxShadow: [BoxShadow(
-            color:AppColors.shadow,
-            offset: Offset(-3, 3),
-            blurRadius: 6,
-            spreadRadius: 4.0
-          ),],
-          borderRadius: BorderRadius.circular(10)
-        ),
-        height: 68.0,
-        width: 320.0,
-        margin: EdgeInsetsDirectional.all(20),
-        padding: EdgeInsets.fromLTRB(16, 12, 18, 12),
-        alignment: Alignment.centerLeft,
-        child: Row(
-          children: [
-            Image.asset(
-              imagepath,
-              width: 50,
-              height: 40
-            ),
-            SizedBox(width: 12.0,),
-            Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-              width: 100.0,
-              child: Text(
-                title,
-                style: TextStyle(
-                  fontWeight: FontWeight.bold
+    return Container(
+      decoration: BoxDecoration(
+      color: AppColors.card,
+        boxShadow: [BoxShadow(
+          color:AppColors.shadow,
+          offset: Offset(-3, 3),
+          blurRadius: 15,
+          spreadRadius: 2.0
+      ),],
+        borderRadius: BorderRadius.circular(10)
+      ),
+      margin: EdgeInsetsDirectional.all(20),
+      child: Material(
+        borderRadius: BorderRadius.circular(10), 
+        color: Colors.transparent,
+        child: InkWell(
+          splashColor: AppColors.splashColorlor,
+          highlightColor: AppColors.card,
+          borderRadius: BorderRadius.circular(10), 
+          onTap: () {},
+          child: Container(
+            height: 68.0,
+            width: 320.0,
+            padding: EdgeInsets.fromLTRB(16, 12, 18, 13),
+            alignment: Alignment.centerLeft,
+            child: Row(
+              children: [
+                Image.asset(
+                  imagepath,
+                  width: 50,
+                  height: 40
                 ),
-              )
-              ),
-              SizedBox(height: 4),
-              Container(
-                child: Text("Explore formulas", 
-                  style: TextStyle(
-                    color: AppColors.subtitle 
+                SizedBox(width: 12.0,),
+                Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                  width: 100.0,
+                  child: Text(
+                    title,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 14.5,
+                    ),
                   )
-                ),
-              )
-            ],),
-            const Spacer(),  
-            Icon(Icons.arrow_forward_ios, size: 18.0,)
-          ],       
+                  ),
+                  SizedBox(height: 2),
+                  Container(
+                    child: Text("Explore formulas", 
+                      style: TextStyle(
+                        color: AppColors.subtitle 
+                      )
+                    ),
+                  )
+                ],),
+                const Spacer(),  
+                Icon(Icons.arrow_forward_ios, size: 18.0,)
+              ],       
+            ),
+          ),
         ),
       ),
     );  
--- lib/theme/app_colors.dart
+++ lib/theme/app_colors.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const background = Color(0xFFF8F9FA);
+  static const background = Color.fromARGB(255, 252, 252, 252);
   static const primary = Color(0xFF1565C0);
   static const primaryButotns = Color(0xFF1976D2);
   static const accent = Color(0xFF00ACC1);
@@ -10,5 +10,6 @@ class AppColors {
   static const text = Color(0xFF212121);
   static const subtitle = Color(0xFF616161);
   static const shadow = Colors.black12;
+  static const splashColorlor = Color.fromARGB(10, 0, 0, 0) ;
 }
 
